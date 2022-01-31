62845ebfae6ee25393f1fa62521d6b14fdce1e15
Paulo Cavalheiro <palflinho@gmail.com>
2022-01-31 15:28:47 -0300

    Desafio versão 0.4

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
+  import { Button } from './Button';
+  import { MovieCard } from './MovieCard';
+  import { api } from '../services/api';
+  
+  interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }
+  
+  interface MovieProps {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }
+
 export function SideBar() {
   // Complete aqui
+  return(
+
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+
+  )
 }
